6d94c84d640a8450b0a71408aaeb8810db1ded56
seongilp <zihado@gmail.com>
2026-05-31 08:37:15 +0900

    feat(pokemon): 타입별 평균 특수공격 컬럼 추가

--- models/pokemon/marts/mart_type_strength.sql
+++ models/pokemon/marts/mart_type_strength.sql
@@ -5,7 +5,8 @@ select
     round(avg(total), 1)    as avg_total,
     round(avg(attack), 1)   as avg_attack,
     round(avg(defense), 1)  as avg_defense,
-    round(avg(speed), 1)    as avg_speed
+    round(avg(speed), 1)    as avg_speed,
+    round(avg(sp_atk), 1)   as avg_sp_atk
 from {{ ref('stg_pokemon') }}
 group by 1
 order by avg_total desc
